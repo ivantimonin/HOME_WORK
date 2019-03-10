1c413a46a7464902d9c477700cb6478b185d65aa
timon <timon@DESKTOP-ANCORQQ>
2019-03-10 22:50:46 +0300

    Сделал первые 2 задания по файлам

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,204 @@ namespace Lesson4
 {
     class Program
     {
+        enum Tasks
+        {
+            Task_1 = 1,
+            Task_2,
+            Task_3,
+            Task_4,
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Выбор задания");
+            Tasks choise = Tasks.Task_2;
+            switch (choise)
+            {
+                case Tasks.Task_1: Get_my_directory();
+                    break;
+                case Tasks.Task_2: Calculation();
+                    break;
+                case Tasks.Task_3:
+                    break;
+                case Tasks.Task_4:
+                    break;
+            }
+            Console.ReadLine();
+
+        }
+        static void Get_my_directory()
+        {
+
+            DirectoryInfo dir0 = new DirectoryInfo(@"D:\УРОКИ С#");
+            Item_name(dir0);
+            void Item_name(DirectoryInfo dir)
+            {
+                Console.WriteLine($"Название начального каталога: {dir.FullName}");
+                foreach (var item1 in dir.GetDirectories())
+                {
+                    Console.WriteLine($"    Название подкаталога:");
+                    Console.WriteLine($"    {item1.Name}");
+                    File_name(item1);
+                    foreach (var item2 in item1.GetDirectories())
+                    {
+                        Console.WriteLine($"        Название подкаталога:");
+                        Console.WriteLine($"        {item2.Name}");
+                        File_name(item2);
+                        Console.WriteLine();
+                        foreach (var item3 in item2.GetDirectories())
+                        {
+                            Console.WriteLine($"            Название подкаталога:");
+                            Console.WriteLine($"            {item3.Name}");
+                            File_name(item3);
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+            void File_name(DirectoryInfo dir)
+            {
+                foreach (var item3 in dir.GetFiles())
+                {
+                    Console.WriteLine($"-Название файла:");
+                    Console.WriteLine(item3.Name);
+                }
+            }
+        }
+        static void Calculation()
+        {
+            {
+                Console.Clear();
+                Console.WriteLine("Вы выбрали задание 2 (Калькулятор)");
+
+                Console.WriteLine("Введите знак производимой операции");
+                while (true)
+                {
+                    string sign = Convert.ToString(Console.ReadLine());
+                    if (sign != "+" && sign != "-" && sign != "*"
+                        && sign != "/" && sign != "^" && sign != "^" && sign != "!")
+                    {
+                        Console.WriteLine("знак производимой операции не поддерживается");
+                        Console.WriteLine("доступны следующие знаки");
+                        Console.WriteLine("+ (Сумма)");
+                        Console.WriteLine("- (Вычитание)");
+                        Console.WriteLine("* (Умножение)");
+                        Console.WriteLine("/ (Деление)");
+                        Console.WriteLine("^ (Возведение в степень)");
+                        Console.WriteLine("! (Факториал)");
+                    }
+                    else
+                    {
+                        if (sign == ("*"))
+                        {                            
+                            double a = Read_a_or_write_a();                            
+                            Console.WriteLine("Введите число b");
+                            double b = Convert.ToDouble(Console.ReadLine());
+                            double rezult = a * b;
+                            Console.WriteLine($"Ответ:{a}*{b} = {rezult}");
+                            Save_solution(rezult);
+                        }
+                        if (sign == ("/"))
+                        {
+                            double a = Read_a_or_write_a();
+                            Console.WriteLine("Введите число b");
+                            double b = Convert.ToDouble(Console.ReadLine());
+                            double rezult = a / b;
+                            Console.WriteLine($"Ответ:{a}/{b} = {rezult}");
+                            Save_solution(rezult);
+                        }
+                        if (sign == ("+"))
+                        {
+                            double a = Read_a_or_write_a();
+                            Console.WriteLine("Введите число b");
+                            double b = Convert.ToDouble(Console.ReadLine());
+                            double rezult = a + b;
+                            Console.WriteLine($"Ответ:{a}+{b} = {rezult}");
+                            Save_solution(rezult);
+                        }
+                        if (sign == ("-"))
+                        {
+                            double a = Read_a_or_write_a();
+                            Console.WriteLine("Введите число b");                            
+                            double b = Convert.ToDouble(Console.ReadLine());
+                            double rezult = a - b;
+                            Console.WriteLine($"Ответ:{a}-{b} = {rezult}");
+                            Save_solution(rezult);
+                        }
+                        if (sign == ("^"))
+                        {
+                            double a = Read_a_or_write_a();
+                            Console.WriteLine("Введите показатель степени");
+                            double b = Convert.ToDouble(Console.ReadLine());
+                            double rezult = Math.Pow(a, b);
+                            Console.WriteLine($"Ответ:{a}^{b} = {rezult}");
+                            Save_solution(rezult);
+                        }
+                        if (sign == ("!"))
+                        {
+                            int a = Convert.ToInt32(Read_a_or_write_a());
+                            int rezult = Factorial(a);
+                            Console.WriteLine($"{a} != {rezult}");
+                            Save_solution(rezult);
+                            int Factorial(int a_0)
+                            {
+                                if (a_0 == 0)
+                                {
+                                    return 1;
+                                }
+                                else
+                                {
+                                    return a_0 * Factorial(a_0 - 1);
+                                }
+                            }
+                            break;
+                        }
+                        break;
+                    }
+                }
+                void Save_solution(double rezult)
+                {
+                    Console.WriteLine("Желаете сохранить решение в файл Y/N?");
+                    char answer = Convert.ToChar(Console.ReadLine());
+                    if (answer == 'Y')
+                    {
+                        string path = @"..\rezult.txt";
+                        if (!File.Exists(path))
+                        {
+                            File.Create(path);
+                        }
+                        StreamWriter write_rez = new StreamWriter(@"..\rezult.txt");
+                        write_rez.WriteLine(rezult);
+                        write_rez.Close();
+                    }                    
+                }
+                double Read_a_or_write_a()
+                {
+                    Console.WriteLine("Желаете чтобы переменная a имела сохраненное решение Y/N?");
+                    char answer = Convert.ToChar(Console.ReadLine());
+                    double rez = 0;
+                    if (answer == 'Y')
+                    {
+                        string path = @"..\rezult.txt";                        
+                        if (!File.Exists(path))
+                        {
+                            Console.WriteLine("Сохраненного решения нет");
+                        }
+                        else
+                        {
+                            rez = Convert.ToDouble(File.ReadAllText(path));
+                        }                       
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите число a");
+                        rez= Convert.ToDouble(Console.ReadLine());
+                    }
+                    return rez;
+
+
+
+                }
+            }
 
         }
     }
